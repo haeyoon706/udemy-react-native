5f6120e0e9fa6dcd04eb7cdec6b5b18aaafb0ce9
developer-heyy <121794924+haeyoon706@users.noreply.github.com>
2024-01-11 00:03:47 +0900

    ✨ state로 화면 전환하기

---
+++ 2-number-game/screens/GameScreen.js
@@ -0,0 +1,7 @@
+import { View, Text } from "react-native";
+
+function GameScreen() {
+  return <Text>Game Screen!</Text>;
+}
+
+export default GameScreen;
--- 2-number-game/screens/StartGameScreen.js
+++ 2-number-game/screens/StartGameScreen.js
@@ -2,7 +2,9 @@ import { View, TextInput, StyleSheet, Alert } from "react-native";
 import { useState } from "react";
 import PrimaryButton from "../components/PrimaryButton";
 
-function StartGameScreen() {
+function StartGameScreen(props) {
+  const { onPickNumber } = props;
+
   const [enteredNumber, setEnteredNumber] = useState("");
 
   function numberInputHandler(enteredText) {
@@ -25,7 +27,7 @@ function StartGameScreen() {
       return;
     }
 
-    console.log("Valid number");
+    onPickNumber(chosenNumber);
   }
 
   return (
