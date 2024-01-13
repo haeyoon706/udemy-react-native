01ace46d4cc0376e193ec02650d25bdef02bc496
developer-heyy <121794924+haeyoon706@users.noreply.github.com>
2024-01-14 00:16:51 +0900

    💄 게임 오버 이미지 스타일링

--- 2-number-game/screens/GameOverScreen.js
+++ 2-number-game/screens/GameOverScreen.js
@@ -1,7 +1,42 @@
-import { Text } from "react-native";
+import { Text, View, Image, StyleSheet } from "react-native";
+import Title from "../components/ui/Title";
+import Colors from "../constants/color";
 
 function GameOverScreen() {
-  return <Text>Game is over!</Text>;
+  return (
+    <View style={styles.rootContainer}>
+      <Title>GAME OVER!</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require("../assets/images/success.png")}
+          style={styles.image}
+        />
+      </View>
+      <Text>Your phone needed X rouds to guess ther number Y.</Text>
+    </View>
+  );
 }
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderCollor: Colors.primary800,
+    overflow: "hidden",
+    margin: 36,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
