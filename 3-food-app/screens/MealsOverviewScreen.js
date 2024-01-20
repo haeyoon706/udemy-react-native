26c94f6e82cffb51a03f6b6b7a52d0e09ddc1a52
developer-heyy <121794924+haeyoon706@users.noreply.github.com>
2024-01-20 14:14:06 +0900

    💄 상세 화면 및 내비게이션 옵션 스타일링

--- 3-food-app/screens/MealsOverviewScreen.js
+++ 3-food-app/screens/MealsOverviewScreen.js
@@ -11,7 +11,17 @@ function MealsOverviewScreen(props) {
   });
 
   function renderMealItem(itemData) {
-    return <MealItem title={itemData.item.title} />;
+    const { item } = itemData;
+
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      duration: item.duration,
+      affordability: item.affordability,
+      complexity: item.complexity,
+    };
+
+    return <MealItem {...mealItemProps} />;
   }
 
   return (
