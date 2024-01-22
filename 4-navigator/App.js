4a93fea207f6b9f0e3b01b6addac755395fa2c9f
haeyoon706 <haeyoon706@gmail.com>
2024-01-23 00:20:34 +0900

    ✨ 하단 탭 추가 및 구성

--- 4-navigator/App.js
+++ 4-navigator/App.js
@@ -1,48 +1,43 @@
 import { Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
 import WelcomScreen from "./screens/WelcomeScreen";
 import UserScreen from "./screens/UserScreen";
 import { Ionicons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-const Drawer = createDrawerNavigator();
+const ButtonTab = createBottomTabNavigator();
+// const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator
+      <Text>test</Text>
+      <ButtonTab.Navigator
         screenOptions={{
-          headerStyle: {
-            backgroundColor: "#3c0a6b",
-          },
+          headerStyle: { backgroundColor: "#3c0a6b" },
           headerTintColor: "white",
-          drawerActiveBackgroundColor: "#f0e1ff",
-          drawerActiveTintColor: "#3c0a6b",
-          drawerStyle: {
-            backgroundColor: "#ccc",
-          },
+          tabBarActiveTintColor: "#3c0a6b",
         }}
       >
-        <Drawer.Screen
+        <ButtonTab.Screen
           name="Welcome"
           component={WelcomScreen}
           options={{
-            drawerLabel: "Welcom Screen",
-            drawerIcon: ({ color, size }) => (
+            tabBarIcon: ({ color, size }) => (
               <Ionicons name="home" color={color} size={size} />
             ),
           }}
         />
-        <Drawer.Screen
+        <ButtonTab.Screen
           name="User"
           component={UserScreen}
           options={{
-            drawerIcon: ({ color, size }) => (
+            tabBarIcon: ({ color, size }) => (
               <Ionicons name="person" color={color} size={size} />
             ),
           }}
         />
-      </Drawer.Navigator>
+      </ButtonTab.Navigator>
     </NavigationContainer>
   );
 }
