3023e8017a12e5165d0074c90988e18ff626a438
haeyoon706 <haeyoon706@gmail.com>
2024-01-22 23:56:13 +0900

    ✨ 드로어 내비게이션 추가 및 생성

--- 4-navigator/App.js
+++ 4-navigator/App.js
@@ -1,5 +1,18 @@
-import { Text } from 'react-native';
+import { Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import WelcomScreen from "./screens/WelcomeScreen";
+import UserScreen from "./screens/UserScreen";
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
-  return <Text>Todo...</Text>;
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Welcome" component={WelcomScreen} />
+        <Drawer.Screen name="User" component={UserScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
 }
