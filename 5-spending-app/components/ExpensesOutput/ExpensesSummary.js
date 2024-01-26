3ff2ddf0fa5bbb2165cda6aa8b39735f562521f4
haeyoon706 <haeyoon706@gmail.com>
2024-01-26 23:55:25 +0900

    ✨ 지출 컴포넌트 구성

--- 5-spending-app/components/ExpensesOutput/ExpensesSummary.js
+++ 5-spending-app/components/ExpensesOutput/ExpensesSummary.js
@@ -1,10 +1,14 @@
 import { View, Text } from "react-native";
 
-function ExpensesSummary() {
+function ExpensesSummary({ expenses, periodName }) {
+  const expensesSum = expenses.reduce((sum, expenses) => {
+    return sum + expenses.amoount;
+  }, 0);
+
   return (
     <View>
-      <Text>Last 7 Days</Text>
-      <Text>$177.95</Text>
+      <Text>{periodName}</Text>
+      <Text>${expensesSum.toFixed(2)}</Text>
     </View>
   );
 }
