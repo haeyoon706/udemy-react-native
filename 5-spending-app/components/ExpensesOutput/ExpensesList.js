39e3d88ae1545b304f261643049b13fbc4cc45c2
haeyoon706 <haeyoon706@gmail.com>
2024-01-27 00:06:13 +0900

    ✨ 더미 지출 데이터 추가 및 출력

--- 5-spending-app/components/ExpensesOutput/ExpensesList.js
+++ 5-spending-app/components/ExpensesOutput/ExpensesList.js
@@ -1,7 +1,18 @@
 import { FlatList } from "react-native";
+import { Text } from "react-native";
 
-function ExpensesList() {
-  return <FlatList />;
+function renderExpensesItem(itemData) {
+  return <Text>{itemData.item.description}</Text>;
+}
+
+function ExpensesList({ expenses }) {
+  return (
+    <FlatList
+      data={expenses}
+      renderItem={renderExpensesItem}
+      keyExtractor={(item) => item.id}
+    />
+  );
 }
 
 export default ExpensesList;
--- 5-spending-app/components/ExpensesOutput/ExpensesOutput.js
+++ 5-spending-app/components/ExpensesOutput/ExpensesOutput.js
@@ -2,11 +2,44 @@ import { View } from "react-native";
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    description: "A pair of shoes",
+    amount: 59.99,
+    date: new Date("2024-01-20"),
+  },
+  {
+    id: "e2",
+    description: "A pair of toursers",
+    amount: 89.29,
+    date: new Date("2023-12-05"),
+  },
+  {
+    id: "e3",
+    description: "Some bananas",
+    amount: 5.99,
+    date: new Date("2023-11-20"),
+  },
+  {
+    id: "e4",
+    description: "A book",
+    amount: 15.99,
+    date: new Date("2023-09-18"),
+  },
+  {
+    id: "e5",
+    description: "Another book",
+    amount: 18.99,
+    date: new Date("2023-09-01"),
+  },
+];
+
 function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
     <View>
-      <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
-      <ExpensesList />
+      <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
+      <ExpensesList expenses={DUMMY_EXPENSES} />
     </View>
   );
 }
