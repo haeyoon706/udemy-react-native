200dc150b22d1ed31b9c15058a80ea1fcb4c101f
haeyoon706 <haeyoon706@gmail.com>
2024-01-28 23:28:55 +0900

    ✨ 지출 삭제 및 더미 데이터 업데이트

--- 5-spending-app/screens/ManageExpenses.js
+++ 5-spending-app/screens/ManageExpenses.js
@@ -1,10 +1,13 @@
-import { useLayoutEffect } from "react";
+import { useContext, useLayoutEffect } from "react";
 import { StyleSheet, View } from "react-native";
 import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/styles";
 import Button from "../components/UI/Button";
+import { ExpensesContext } from "../store/expenses-context";
 
 function ManageExpenses({ route, navigation }) {
+  const expenseCtx = useContext(ExpensesContext);
+
   const editedExpenseId = route.params?.expenseId;
   const isEditing = !!editedExpenseId;
 
@@ -15,6 +18,7 @@ function ManageExpenses({ route, navigation }) {
   }, [navigation, isEditing]);
 
   function deleteExpenseHandler() {
+    expenseCtx.deleteExpense(editedExpenseId);
     navigation.goBack();
   }
 
@@ -23,6 +27,19 @@ function ManageExpenses({ route, navigation }) {
   }
 
   function confirmHandler() {
+    if (isEditing) {
+      expenseCtx.updateExpense(editedExpenseId, {
+        description: "Test!!!",
+        amount: 29.99,
+        date: new Date("2024-01-28"),
+      });
+    } else {
+      expenseCtx.addExpense({
+        description: "Test",
+        amount: 19.99,
+        date: new Date("2024-01-28"),
+      });
+    }
     navigation.goBack();
   }
 
