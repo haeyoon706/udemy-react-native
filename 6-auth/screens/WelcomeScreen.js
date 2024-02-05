0f31a8d27a057dfbcbfbcf102e218550d57f64d5
haeyoon706 <haeyoon706@gmail.com>
2024-02-05 23:27:23 +0900

    ✨ api 요청 시 토큰 전송

--- 6-auth/screens/WelcomeScreen.js
+++ 6-auth/screens/WelcomeScreen.js
@@ -1,10 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../store/auth-context";
 
 function WelcomeScreen() {
+  const authCtx = useContext(AuthContext);
+  const token = authCtx;
+
+  const [fetchedMessage, setFetchedMessage] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://react-native-course-53a2c-default-rtdb.firebaseio.com/message.json?auth=" +
+          token
+      )
+      .then((res) => {
+        setFetchedMessage(res.data);
+      });
+  }, [token]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
@@ -14,13 +34,13 @@ export default WelcomeScreen;
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 32,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 8,
   },
 });
