abfad39d06e950565fd3c4acab37d67f1e6b1bc2
haeyoon706 <haeyoon706@gmail.com>
2024-02-11 11:19:44 +0900

    ✨ Android에서 사진 찍기

--- 7-my-place-app/components/Places/ImagePicker.js
+++ 7-my-place-app/components/Places/ImagePicker.js
@@ -1,7 +1,22 @@
-import { View } from "react-native";
+import { View, Button } from "react-native";
+import { launchCameraAsync } from "expo-image-picker";
 
 function ImagePicker() {
-  return <View></View>;
+  async function takeImageHandler() {
+    const image = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+    console.log(image);
+  }
+
+  return (
+    <View>
+      <View></View>
+      <Button title="Take Image" onPress={takeImageHandler} />
+    </View>
+  );
 }
 
 export default ImagePicker;
