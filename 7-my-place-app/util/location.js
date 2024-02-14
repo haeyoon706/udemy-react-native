6f84986eac01661c95b3dafa26e6c017706620fb
haeyoon706 <haeyoon706@gmail.com>
2024-02-14 23:29:35 +0900

    ✨ 선택한 장소 추가 페이지로 넘기기

--- 7-my-place-app/util/location.js
+++ 7-my-place-app/util/location.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyBJPujQd0zH";
+const GOOGLE_API_KEY = "AIzaSyBJPujQd0zHtCoXgENIbedIg1IKiPgTYAg";
 
 export function getMapPreview(lat, lng) {
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap
