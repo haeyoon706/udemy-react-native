a88dfccfe3fe232f5c803f6775a24b80812bfbf8
haeyoon706 <haeyoon706@gmail.com>
2024-02-15 22:09:50 +0900

    ✨ 장소 좌표값 주소로 변환

--- 7-my-place-app/components/Places/LocationPicker.js
+++ 7-my-place-app/components/Places/LocationPicker.js
@@ -7,7 +7,7 @@ import {
   PermissionStatus,
 } from "expo-location";
 import { useEffect, useState } from "react";
-import { getMapPreview } from "../../util/location";
+import { getAddress, getMapPreview } from "../../util/location";
 import {
   useNavigation,
   useRoute,
@@ -35,7 +35,17 @@ function LocationPicker({ onPickLocation }) {
   }, [route, isFocused]);
 
   useEffect(() => {
-    onPickLocation(pickedLocation);
+    async function handleLocation() {
+      if (pickedLocation) {
+        const address = await getAddress(
+          pickedLocation.lat,
+          pickedLocation.lng
+        );
+        onPickLocation({ ...pickedLocation, address: address });
+      }
+    }
+
+    handleLocation();
   }, [pickedLocation, onPickLocation]);
 
   async function verifyPermissions() {
