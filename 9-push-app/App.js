0f2b983c22bd2e3a9933c4362133c59c6e148c2a
haeyoon706 <haeyoon706@gmail.com>
2024-02-27 21:02:38 +0900

    ✨ 로컬 알림 이벤트 리스너 추가

--- 9-push-app/App.js
+++ 9-push-app/App.js
@@ -13,7 +13,7 @@ Notifications.setNotificationHandler({
 
 export default function App() {
   useEffect(() => {
-    const subscription = Notifications.addNotificationReceivedListener(
+    const subscription1 = Notifications.addNotificationReceivedListener(
       (notification) => {
         console.log("NOTIFICATION RECEIVED");
         console.log(notification);
@@ -22,8 +22,18 @@ export default function App() {
       }
     );
 
+    const subscription2 = Notifications.addNotificationResponseReceivedListener(
+      (response) => {
+        console.log("NOTIFICATION RESPONSE RECEIVED");
+        console.log(response);
+        const userName = response.notification.request.content.data.userName;
+        console.log(userName);
+      }
+    );
+
     return () => {
-      subscription.remove();
+      subscription1.remove();
+      subscription2.remove();
     };
   }, []);
 
